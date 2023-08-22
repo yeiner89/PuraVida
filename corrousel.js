bb5f0270bfc0b5f2e854150e109777e4738a48da
yeiner apa <yeinerlop89@hotmail.com>
2023-08-22 11:31:09 -0600

    adding chatgpt and different style

---
+++ corrousel.js
@@ -0,0 +1,103 @@
+/*carrousel*/
+
+$(document).ready(function () {
+  var count_group = $(".list .item").length;
+  var active = 0;
+  var deg_bg = 0;
+  var begin = true;
+
+  function Load() {
+    if (begin) {
+      begin = false;
+    } else {
+      $(
+        "#item_" +
+          (active - 2 < -1
+            ? count_group - 2
+            : active - 2 < 0
+            ? count_group - 1
+            : active - 2)
+      ).removeClass("hide");
+      $("#item_" + (active - 1 < 0 ? count_group - 1 : active - 1))
+        .removeClass("active")
+        .addClass("hide");
+    }
+
+    $("#item_" + active)
+      .removeClass("hide")
+      .addClass("active");
+
+    $(".dots-page div").removeClass("active");
+    $("#dot_" + active).addClass("active");
+    $(".effect").addClass("start");
+    beginPosition();
+
+    setTimeout(function () {
+      $(".effect").removeClass("start");
+    }, 3000);
+  }
+
+  Load();
+
+  $("#next").on("click", function () {
+    active = active + 1 >= count_group ? 0 : active + 1;
+    Load();
+  });
+
+  $("#back").on("click", function () {
+    $("#item_" + active)
+      .removeClass("active")
+      .addClass("hide");
+    active = active - 1 < 0 ? count_group - 1 : active - 1;
+    Load();
+  });
+});
+
+$(".button").hover(
+  function () {
+    if ($(".effect").hasClass("start")) {
+      return;
+    }
+    $(".birt").css("right", "31%");
+    $(".birt").css("bottom", "16%");
+    $(".birt").css("transform", "rotateY(192deg) rotate(-97deg)");
+  },
+  function () {
+    beginPosition();
+  }
+);
+
+function beginPosition() {
+  $(".birt").css("right", "50%");
+  $(".birt").css("bottom", "25%");
+  $(".birt").css("transform", "rotateY(192deg) rotate(-40deg)");
+}
+
+$(".item").hover(
+  function (event) {
+    var $PosTop = event.pageY;
+    var $PosLeft = event.pageX;
+    var $height = $(".item").height();
+    var $width = $(".item").width();
+    var defaultTop = 25;
+    var defaultLeft = 50;
+    var topNew = $PosTop > $height / 2 + 30 ? defaultTop - 10 : defaultTop + 10;
+    var leftNew =
+      $PosLeft > $width / 3 + 30 ? defaultLeft - 10 : defaultLeft + 10;
+    $(".birt").css("right", leftNew + "%");
+    $(".birt").css("bottom", topNew + "%");
+    console.log($PosTop, $height / 2 + 30);
+  },
+  function () {
+    beginPosition();
+  }
+);
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    // Add your transition code here
+  });
+});
--- script.js
+++ script.js
@@ -1,89 +1,86 @@
-$(document).ready(function() {
-    var count_group = $(".list .item").length;
-    var active = 0;
-    var deg_bg = 0;
-    var begin = true;
+const apiKey = "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";
+const apiUrl = "https://api.openai.com/v1/engines/text-davinci-003/completions";
 
-    function Load() {
-        if (begin) {
-            begin = false;
-        } else {
-            $('#item_' + (active - 2 < -1 ? count_group - 2 : active - 2 < 0 ? count_group - 1 : active - 2)).removeClass('hide');
-            $('#item_' + (active - 1 < 0 ? count_group - 1 : active - 1)).removeClass('active').addClass('hide');
-        }
+const chatBox = document.getElementById("chat-box");
+const userInput = document.getElementById("user-input");
+const sendButton = document.getElementById("send-button");
+const loadingIndicator = document.getElementById("loading-indicator");
+const replyBox = document.getElementById("reply-box");
 
-        $('#item_' + active).removeClass('hide').addClass('active');
-
-        $('.dots-page div').removeClass('active');
-        $('#dot_' + active).addClass('active');
-        $('.effect').addClass('start');
-        beginPosition();
-
-        setTimeout(function() {
-            $('.effect').removeClass('start');
-        }, 3000);
-    }
-
-    Load();
-
-    $('#next').on('click', function() {
-        active = active + 1 >= count_group ? 0 : active + 1;
-        Load();
-    });
-
-    $('#back').on('click', function() {
-        $('#item_' + active).removeClass('active').addClass('hide');
-        active = active - 1 < 0 ? count_group - 1 : active - 1;
-        Load();
-    });
+userInput.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    sendButton.click();
+  }
 });
 
-$('.button').hover(function(){
-    if($(".effect").hasClass("start")){
-        return;
-    }
-        $('.birt').css('right','31%');
-        $('.birt').css('bottom','16%');
-        $('.birt').css('transform','rotateY(192deg) rotate(-97deg)');
-}
-,function(){
-       beginPosition();
+sendButton.addEventListener("click", async () => {
+  const userMessage = userInput.value;
+  if (userMessage.trim() === "") return;
+
+  displayMessage(userMessage, "user-message");
+  toggleLoadingIndicator(true);
+
+  try {
+    const aiResponse = await getChatResponse(userMessage);
+    displayReply(aiResponse);
+  } catch (error) {
+    displayErrorMessage("An error occurred. Please try again.");
+  } finally {
+    toggleLoadingIndicator(false);
+  }
+
+  userInput.value = "";
 });
 
-function beginPosition(){
-    $('.birt').css('right','50%');
-    $('.birt').css('bottom','25%');
-    $('.birt').css('transform','rotateY(192deg) rotate(-40deg)');
+async function getChatResponse(inputText) {
+  const response = await fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      prompt: inputText,
+      max_tokens: 250, // Adjust as needed
+      // Specify the model
+    }),
+  });
+
+  const data = await response.json();
+  return data.choices[0].text;
 }
 
-$('.item').hover(function(event){
-    var $PosTop = event.pageY;
-    var $PosLeft = event.pageX;
-    var $height = $('.item').height();
-    var $width = $('.item').width();
-    var defaultTop = 25;
-    var defaultLeft = 50;
-    var topNew = $PosTop > ( $height/2) + 30 ? defaultTop - 10 :  defaultTop + 10;
-    var leftNew = $PosLeft > ( $width/ 3) + 30 ? defaultLeft - 10 :   defaultLeft +10;
-    $('.birt').css('right',leftNew +'%');
-    $('.birt').css('bottom', topNew +'%');
-    console.log($PosTop, ( $height/2) + 30);
+function displayMessage(message, messageType) {
+  const messageDiv = document.createElement("div");
+  messageDiv.textContent = message;
+  messageDiv.classList.add("message", messageType);
+  chatBox.appendChild(messageDiv);
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
 
-}, function(){
-    beginPosition();
-});
+function toggleLoadingIndicator(show) {
+  loadingIndicator.style.display = show ? "block" : "none";
+  sendButton.disabled = show;
+}
 
-const navLinks = document.querySelectorAll('.nav-link');
+function displayReply(reply) {
+  const replyLines = reply.split("\n"); // Split the response by line breaks
 
-navLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        // Add your transition code here
-    });
-});
+  replyBox.innerHTML = ""; // Clear previous replies
 
+  for (const line of replyLines) {
+    const replyDiv = document.createElement("div");
+    replyDiv.textContent = line;
+    replyDiv.classList.add("reply-message");
+    replyBox.appendChild(replyDiv);
+  }
+}
 
-
-
-
-  
+function displayErrorMessage(message) {
+  const errorDiv = document.createElement("div");
+  errorDiv.textContent = message;
+  errorDiv.classList.add("message", "error-message");
+  chatBox.appendChild(errorDiv);
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
